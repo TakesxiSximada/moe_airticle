969d7cc511367fc34222b8c7dc7de60bc46ac2be
TakesxiSximada <takeshi.shimada@ticketstar.jp>
2014-06-01 14:48:15 +0900

    typo

--- moe_article/manage.py
+++ moe_article/manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.asbpaht(__file__)))
+sys.path.append(os.path.dirname(os.path.abspaht(__file__)))
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moe_article.settings")
