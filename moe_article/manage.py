07c127563e4843f7e6aa408c748b35b798e8984c
TakesxiSximada <takeshi.shimada@ticketstar.jp>
2014-06-01 14:47:02 +0900

    pathと押し2

--- moe_article/manage.py
+++ moe_article/manage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
+sys.path.append(os.path.dirname(os.path.asbpaht(__file__)))
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moe_article.settings")
