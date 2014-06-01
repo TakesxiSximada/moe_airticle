d793d345b488bbff1e2cdfb98a041a1f1b50b0f7
TakesxiSximada <takeshi.shimada@ticketstar.jp>
2014-06-01 14:43:50 +0900

    pathを通してみた

--- moe_article/moe_article/settings.py
+++ moe_article/moe_article/settings.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import sys
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
